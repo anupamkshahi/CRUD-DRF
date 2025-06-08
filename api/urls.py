eb55dbd37b4b76c9e71c849049880a8744db337d
Anupam Shahi <anupamkshahi@gmail.com>
2025-06-08 21:05:53 +0530

    Initial commit

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from expense import views
 
 urlpatterns = [
-    path('get-transactions/',views.get_transactions)
+    path('get-transactions/',views.get_transactions),
+    path('transactions/',views.TransactionAPI.as_view())
 ]
